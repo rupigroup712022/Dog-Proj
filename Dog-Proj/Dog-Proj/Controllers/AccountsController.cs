f5c31fb9e65b1b27fbefe8e1cd6a1cc9e2e4ad30
chen0308 <chen0308sch@gmail.com>
2022-03-14 18:29:05 +0200

    che2
    1

---
+++ Dog-Proj/Dog-Proj/Controllers/AccountsController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Dog_Proj.Controllers
+{
+    public class AccountsController : ApiController
+    {
+     
+        // GET api/<controller>
+        public Accounts Get(string Email, string Password)
+        {
+            Accounts Accounts = new Accounts();
+            return user.ReadUser(Email, Password);
+        }
+
+
+        // GET api/<controller>/5
+        //public string Get(int id)
+        //{
+        //    return "value";
+        //}
+
+        // POST api/<controller>
+        public int Post([FromBody]Accounts accounts)
+        {
+            return accounts.Insert();
+        }
+
+        //public int Put(int id)
+        //{
+        //    Accounts accounts = new Accounts();
+        //    return accounts.UpdateAccounts(id);
+        //}
+
+        //// DELETE api/<controller>/5
+        //public void Delete(int id)
+        //{
+        //    Accounts accounts = new Accounts();
+        //    accounts.DeleteAccounts(id);
+        //}
+    }
+}
